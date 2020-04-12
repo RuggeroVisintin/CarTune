5f8ed29b3a34ceaed7ccbaf27ed4932af4c66a7f
Ruggero Visintin <ruggerovisintin@outlook.it>
2020-04-12 19:37:05 +0200

    Added background cubemap

--- src/CarTune.ts
+++ src/CarTune.ts
@@ -1,9 +1,11 @@
-import { WebGLRenderer, PerspectiveCamera, Scene, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { WebGLRenderer, PerspectiveCamera, Scene, CubeTextureLoader, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import {OrbitControls} from 'three-orbitcontrols-ts';
 
 export class CarTune {
     private _renderer: WebGLRenderer;
     private _camera: PerspectiveCamera;
     private _scene: Scene;
+    private _controls: OrbitControls;
 
     constructor() {
         this._renderer = new WebGLRenderer();
@@ -12,19 +14,32 @@ export class CarTune {
         this._camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         this._camera.position.z = 5;
 
-        this._scene = new Scene();
         var geometry = new BoxGeometry;
 		var material = new MeshBasicMaterial( { color: 0x00ff00 } );
         var cube = new Mesh( geometry, material );
-        
+
+        this._scene = new Scene();
         this._scene.add(cube);
 
+        this._scene.background = new CubeTextureLoader()
+            .setPath('assets/img/default_cubemap/')
+            .load([
+                'posx.jpg',
+                'negx.jpg',
+                'posy.jpg',
+                'negy.jpg',
+                'posz.jpg',
+                'negz.jpg'
+            ]);
+
+        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
         document.body.appendChild(this._renderer.domElement);
     }
 
     start() {
         requestAnimationFrame(() => {this.start()});
 
+        this._controls.update();
         this._renderer.render(this._scene, this._camera);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './index.ts',
+  devtool: 'source-map',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.ts?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
     ],
   },
@@ -18,4 +21,10 @@ module.exports = {
     filename: 'CarTune.js',
     path: path.resolve(__dirname, 'build'),
   },
+  plugins: [
+    new CopyPlugin([
+      { from: './assets', to:  path.resolve(__dirname, 'build') + '/assets' },
+      { from: 'index.html', to:  path.resolve(__dirname, 'build') }
+    ]),
+  ],
 };
